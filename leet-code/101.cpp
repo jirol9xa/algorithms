44af73ff266a7652c8ce5c6323ae1995952af8dc
Трунов Владимир Владимирович <vtrunov@ispras.ru>
2023-07-31 11:16:51 +0300

    Fib1 and Fib2

--- leet-code/101.cpp
+++ leet-code/101.cpp
@@ -6,26 +6,29 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right)
+ * {}
  * };
  */
 class Solution {
-public:
-    bool isSymmetric(TreeNode* root) {
+  public:
+    bool isSymmetric(TreeNode *root)
+    {
         if (!root)
             return true;
 
         return isSubTreeEqual(root->left, root->right);
     }
 
-    bool isSubTreeEqual(TreeNode *first, TreeNode *second) {
+    bool isSubTreeEqual(TreeNode *first, TreeNode *second)
+    {
         if (!first && !second)
             return true;
         if (!first || !second)
             return false;
-        
-        return  first->val == second->val && 
-                isSubTreeEqual(first->left,  second->right) && 
-                isSubTreeEqual(first->right, second->left);
+
+        return first->val == second->val && isSubTreeEqual(first->left, second->right) &&
+               isSubTreeEqual(first->right, second->left);
     }
-};;
+};
+;
---
+++ yandex-handbook/Fib1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std::cin >> n;
+    if (n == 0) {
+        std::cout << 0;
+        return 0;
+    }
+
+    int prev = 0;
+    int cur  = 1;
+    for (int i = 1; i < n; ++i) {
+        int tmp = cur;
+        cur += prev;
+        prev = tmp;
+    }
+
+    std::cout << cur;
+
+    return 0;
+}
